9f255717246bb4d5422fd80fb55a6e9730fb53db
Michael Kardakov <work.zjiodeu@gmail.com>
2020-08-01 11:31:43 +0300

    Finish rating feature

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,7 @@ use App\Entity\Product;
 use App\Entity\Rating;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,12 +24,16 @@ class ProductController extends AbstractController
     public function addVote(Product $product, Request $request, EntityManagerInterface $em)
     {
         $voteVal = $request->request->get('vote');
+        $token = $request->request->get('token');
+        if (!$this->isCsrfTokenValid('add_to_cart', $token)) {
+            throw new BadRequestException('Wrong CSRF token. Are you robot?');
+        }
         $vote = new Rating();
         $vote->setValue($voteVal);
         $product->addRating($vote);
         $em->persist($vote);
         $em->persist($product);
         $em->flush();
-        return new Response();
+        return new Response($product->getCurrentRating());
     }
 }
